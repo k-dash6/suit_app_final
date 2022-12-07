f14087389edde8e1d35fbb70ea838d40a9d563a4
k-dash6 <dakozlova_1@edu.hse.ru>
2022-12-08 01:36:26 +0300

    fix import

--- main_app/manage_ontology/views.py
+++ main_app/manage_ontology/views.py
@@ -7,7 +7,6 @@ from .forms import *
 
 from rdflib import URIRef, BNode, Literal, Namespace, Graph, RDF
 from rdflib.namespace import FOAF, DCTERMS, XSD
-
 from main_app.main_app.settings import BASE_DIR
 
 
