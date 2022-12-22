87ca096cff9e6a39800fddd8e87dc87adefdaa3f
k-dash6 <dakozlova_1@edu.hse.ru>
2022-12-22 21:11:16 +0300

    move path

--- main_app/manage_ontology/views.py
+++ main_app/manage_ontology/views.py
@@ -7,7 +7,8 @@ from .forms import *
 
 from rdflib import URIRef, BNode, Literal, Namespace, Graph, RDF
 from rdflib.namespace import FOAF, DCTERMS, XSD
-from main_app.main_app.settings import BASE_DIR
+from pathlib import Path
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 def home(request):
