f3c9b19c53f53e01f47f9bdc4c3e555ad738b61c
k-dash6 <dakozlova_1@edu.hse.ru>
2023-05-03 12:43:34 +0300

    add new ontology

--- main_app/manage_ontology/views.py
+++ main_app/manage_ontology/views.py
@@ -2,13 +2,16 @@ import os
 import random
 
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from .forms import *
 
 from rdflib import URIRef, BNode, Literal, Namespace, Graph, RDF
 from rdflib.namespace import FOAF, DCTERMS, XSD
 from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
+import json
+import ontor
+from rdflib import URIRef, BNode, Literal, Namespace, Graph, RDF
 
 
 def home(request):
@@ -125,6 +128,64 @@ def test_return_random(a):
     return random.choice(sub_list)
 
 
-def visual(request):
+def get_updated_nodes_and_edges():
+    ontor3 = ontor.OntoEditor("CostumesRDF.owl", "CostumesRDF.owl")
+    magic_list_0 = [p.name for p in ontor3.get_elems()[0]]
+    magic_list_1 = [p.name for p in ontor3.get_elems()[1]]
+    magic_list_3 = [p.name for p in ontor3.get_elems()[3]]
 
+    nodes = magic_list_0 + magic_list_3
+    edges = magic_list_1
+
+    data_nodes = []
+    for i in range(len(nodes)):
+        data_nodes.append({
+            'id': i,
+            'label': nodes[i]
+        })
+
+    nodes_dict = {}
+    for d in data_nodes:
+        nodes_dict[d['label']] = d['id']
+
+    g = Graph()
+    g.parse("CostumesRDFfromapp.owl")
+
+    magic_dict = {}
+    data_edges = []
+
+    for node_name in nodes:
+        sub = URIRef('http://www.semanticweb.org/masha/ontologies/2022/9/Сostumes.owl#' + node_name)
+        for s, p, o in g.triples((sub, None, None)):
+            if '#type' in p or '#NamedIndividual' in o or '#' not in p or '#' not in o:
+                continue
+            from_node_name = s.split('#')[1]
+            to_node_name = o.split('#')[1]
+            edge_name = p.split('#')[1]
+            magic_dict = {'from': from_node_name, 'to': to_node_name, 'label': edge_name}
+            data_edges.append(magic_dict)
+
+    for d in data_edges:
+        for key, value in d.items():
+            if key == 'label':
+                continue
+            d[key] = nodes_dict[value]
+
+    return data_edges, data_nodes
+
+
+# def visual(request):
+#     return render(request, 'manage_ontology/ontology_visualization.html')
+
+def nodes_request():
+    _, nodes = get_updated_nodes_and_edges()
+    response = JsonResponse(nodes)
+    return response
+
+def edges_request():
+    edges, _ = get_updated_nodes_and_edges()
+    response = JsonResponse(edges)
+    return response
+
+def visual(request):
     return render(request, 'manage_ontology/ontology_visualization.html')
