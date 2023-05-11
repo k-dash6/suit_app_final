9a74f0a1f7dfe5845b00586dba1a8efc3292a6e5
k-dash6 <dakozlova_1@edu.hse.ru>
2023-05-11 15:05:37 +0300

    try to fix auth

--- main_app/manage_ontology/views.py
+++ main_app/manage_ontology/views.py
@@ -693,7 +693,7 @@ def user_login(request):
             user = authenticate(request, email=email, password=password)
             if user is not None:
                 login(request, user)
-                return redirect
+                return redirect('home')
 
 
 def user_logout(request):
